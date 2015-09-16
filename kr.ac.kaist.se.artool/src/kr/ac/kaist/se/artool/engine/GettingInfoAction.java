996f09a971da4dbb4e2266775a30d72c7b928a4f
Ahrim Han <ahrimhan@gmail.com>
2015-09-16 21:12:27 +0900

    Getting initial fitnesses.

--- kr.ac.kaist.se.artool/src/kr/ac/kaist/se/artool/engine/GettingInfoAction.java
+++ kr.ac.kaist.se.artool/src/kr/ac/kaist/se/artool/engine/GettingInfoAction.java
@@ -1,19 +1,35 @@
 package kr.ac.kaist.se.artool.engine;
 
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 
 import kr.ac.kaist.se.aom.AbstractObjectModel;
+import kr.ac.kaist.se.artool.search.ARSearchMain;
+import kr.ac.kaist.se.artool.search.ARSearchMain.CandidateSelectionType;
+import kr.ac.kaist.se.artool.search.ARSearchMain.FitnessType;
+import kr.ac.kaist.se.artool.search.ARSearchMain.SearchTechType;
+import kr.ac.kaist.se.artool.search.fitness.ConnectivityEngine;
+import kr.ac.kaist.se.artool.search.fitness.EPMEngine;
+import kr.ac.kaist.se.artool.search.fitness.MPCEngine;
+import kr.ac.kaist.se.artool.util.CommandExecutionOperation;
 
+import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.transaction.TransactionalEditingDomain;
+import org.eclipse.gmf.runtime.common.core.command.CommandResult;
+import org.eclipse.gmf.runtime.emf.commands.core.command.AbstractTransactionalCommand;
 import org.eclipse.gmf.runtime.emf.core.GMFEditingDomainFactory;
 import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.ProgressMonitorDialog;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.WizardDialog;
@@ -50,11 +66,61 @@ public class GettingInfoAction implements IObjectActionDelegate {
 		affectedFileList.add(selectedFile);
 		
 		
-		AbstractObjectModel aom = (AbstractObjectModel) aomResource.getContents().get(0);
+		final AbstractObjectModel aom = (AbstractObjectModel) aomResource.getContents().get(0);
 		
 		SystemEntitySet ses = new SystemEntitySet(aom);
 		
-		System.err.println("CEMF: "+aom.getClasses().size() + "," + ses.entities.size() + "," + ses.methods.size() + "," + ses.fields.size() + ")");
+		System.out.println("Class#, "+aom.getClasses().size());
+		System.out.println("Entity#, " + ses.entities.size());
+		System.out.println("Method#, " + ses.methods.size());
+		System.out.println("Field#, "+ ses.fields.size());
+		
+
+		
+		
+		AbstractTransactionalCommand command = new AbstractTransactionalCommand(
+				myEditingDomain, "Measuring Metric Suites", affectedFileList) { //$NON-NLS-1$
+			protected CommandResult doExecuteWithResult(
+					IProgressMonitor monitor, IAdaptable info)
+					throws ExecutionException {
+				try {
+					ConnectivityEngine ce = new ConnectivityEngine(aom);
+					System.out.println("Connectivity, " + ce.calculate());
+					
+					EPMEngine epme = new EPMEngine(ses);
+					System.out.println("EPM, " + epme.calculate());
+					
+					MPCEngine mpce = new MPCEngine(aom);
+					System.out.println("MPC, " + mpce.calculate());			
+				} finally {
+					monitor.done();
+				}
+				
+				return CommandResult.newOKCommandResult();
+			}
+		};
+		
+		
+		
+		
+		
+		try {
+			ProgressMonitorDialog dialog = new ProgressMonitorDialog(shell);
+			
+			dialog.run(true, false, new CommandExecutionOperation(command));
+//			this.mySelectedProjectList.get(0).getProject().refreshLocal(2,
+//					new NullProgressMonitor());
+			// IDE.openEditor(myWorkbenchPage, umlFile);
+		} catch (InvocationTargetException e) {
+			e.printStackTrace();
+			System.err.println(
+					"Unable to create model and diagram"); //$NON-NLS-1$
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+			System.err.println(
+					"Unable to create model and diagram"); //$NON-NLS-1$
+		}
+		
 	}
 
 	@Override
