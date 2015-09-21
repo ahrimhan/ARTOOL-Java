75c1d4b093425c80c36fcfbef76c2d789dea0261
Ahrim Han <ahrimhan@gmail.com>
2015-09-21 19:01:48 +0900

    Native EPM Engine is added.

---
+++ kr.ac.kaist.se.artool/src/kr/ac/kaist/se/artool/search/fitness/NativeEPMEngine.java
@@ -0,0 +1,27 @@
+package kr.ac.kaist.se.artool.search.fitness;
+
+public class NativeEPMEngine {
+	static
+	{
+		System.err.println("Native EPM Engine loading....");
+		System.loadLibrary("libNativeEPMEngine.dylib");
+		System.err.println("Native EPM Engine loading complete.");
+	}
+
+	private long context;
+
+	public NativeEPMEngine()
+	{
+		context = 0;
+	}
+	
+    public native void startToInitialize(int classSize, int methodSize, int fieldSize);
+    public native void setInitialLinkMatrix(int fromEntityId, int toEntityId, float value);
+    public native void setInitialMembershipMatrix(int entityId, int classId, float value);
+    public native void doneToInitialize();
+
+    public native void moveMethod(int fromClassId, int methodId, int toClassId);
+    public native float calculateFitness();
+
+    public native void dispose();
+}
---
+++ kr.ac.kaist.se.artool/src/kr/ac/kaist/se/artool/search/fitness/NativeEPMEngineAdapter.java
@@ -0,0 +1,98 @@
+package kr.ac.kaist.se.artool.search.fitness;
+
+
+import kr.ac.kaist.se.aom.staticmodel.StaticFieldAccess;
+import kr.ac.kaist.se.aom.staticmodel.StaticMethodCall;
+import kr.ac.kaist.se.aom.structure.AOMClass;
+import kr.ac.kaist.se.aom.structure.AOMField;
+import kr.ac.kaist.se.aom.structure.AOMMethod;
+import kr.ac.kaist.se.artool.engine.SystemEntitySet;
+import kr.ac.kaist.se.artool.search.MoveMethodEventListener;
+
+
+public class NativeEPMEngineAdapter extends FitnessFunction implements MoveMethodEventListener {
+
+  
+    private SystemEntitySet ses;
+    private NativeEPMEngine nee;
+    
+    public NativeEPMEngineAdapter(SystemEntitySet ses)
+    {
+        this.ses = ses;
+        this.nee = new NativeEPMEngine();
+        
+        System.err.println("input matrix");
+        nee.startToInitialize(ses.classes.size(), ses.methods.size(), ses.fields.size());
+        initMembershipMatrix();
+        initEntityMatrix();
+        System.err.println("create matrix..");
+
+        nee.doneToInitialize();
+        System.err.println("done to create matrix..");
+    }
+    
+    private void initMembershipMatrix()
+	{
+		for( AOMClass clazz : ses.classes )
+		{
+			for( AOMMethod method : clazz.getMethods() )
+			{
+				nee.setInitialMembershipMatrix(method.getIndex(), clazz.getIndex(), 1);
+			}
+			
+			for( AOMField field : clazz.getFields() )
+			{
+				nee.setInitialMembershipMatrix(field.getIndex(), clazz.getIndex(), 1);
+			}
+		}
+
+	}
+	
+	private void initEntityMatrix()
+	{		
+		for (AOMMethod method : ses.methods) {
+			
+			if( method.getOwnedScope() != null )
+			{
+				for(StaticFieldAccess sfa : method.getOwnedScope().getStaticFieldAccesses() )
+				{
+//					entityMatrix.set(sfa.getAccessedField().getIndex(), method.getIndex(), 1);
+					nee.setInitialLinkMatrix(sfa.getAccessedField().getIndex(), method.getIndex(), 1);
+//					nee.setInitialLinkMatrix(method.getIndex(), sfa.getAccessedField().getIndex(), 1);
+				}
+
+				for(StaticMethodCall smc : method.getOwnedScope().getStaticMethodCalls() )
+				{
+//					entityMatrix.set(method.getIndex(), smc.getCallee().getIndex(), 1);
+					nee.setInitialLinkMatrix(method.getIndex(), smc.getCallee().getIndex(), 1);
+				}
+			}
+		}		
+	}
+	
+
+
+    
+    
+    @Override
+    public void finalize()
+    {
+    	nee.dispose();
+    }
+
+	@Override
+	public void moveMethodPerformed(AOMClass fromClass, AOMMethod method,
+			AOMClass toClass, boolean isRollbackAction) {
+		nee.moveMethod(fromClass.getIndex(), method.getIndex(), toClass.getIndex());
+	}
+
+	@Override
+	public float calculate() {
+		return nee.calculateFitness();
+	}
+
+	@Override
+	public boolean isBiggerValueMeantBetterFitness() {
+		return false;
+	}
+}
